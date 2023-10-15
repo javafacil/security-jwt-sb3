356faf03f3c85a1f71ed64a0e2dc00faaf8d8d8c
Eduardo C <javaphpscala@gmail.com>
2023-10-15 16:41:30 -0500

    first commit: pom, properties and user entity created

---
+++ src/main/java/com/eduardo/securityandjwt/entity/UserEntity.java
@@ -0,0 +1,37 @@
+package com.eduardo.securityandjwt.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "t_user")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Email
+    @NotBlank
+    @Size(max = 80)
+    private String email;
+
+    @NotBlank
+    @Size(max = 30)
+    private String username;
+
+    @NotBlank
+    private String password;
+
+
+}
---
+++ src/test/java/com/eduardo/securityandjwt/SecurityandjwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardo.securityandjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityandjwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
